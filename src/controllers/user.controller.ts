5fdd05af8e24d086b40a75390c47b2470ca53dc7
yogui-dev <jorge.m.arias.p@gmail.com>
2025-06-30 20:30:26 -0400

    feat(rutas): implementa endpoint para eliminar usuarios
    - Agrega ruta DELETE /users/:id para eliminar usuarios
    - Integra el controlador de usuarios con el método delete
    - Actualiza las exportaciones del router para incluir la nueva ruta

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,11 +1,39 @@
 import { Request, Response } from "express";
 
 class UserController {
-  static async getUsers(_req: Request, res: Response) {
+  static async get(_req: Request, res: Response) {
     res.json([
       { id: 1, name: "Jorge" },
       { id: 2, name: "Lady" },
     ]);
+  }
+
+  static async find(_req: Request, res: Response) {
+    res.json({
+      id: 1,
+      name: "Jorge",
+    });
+  }
+
+  static async create(_req: Request, res: Response) {
+    res.json({
+      id: 1,
+      name: "Jorge",
+    });
+  }
+
+  static async update(_req: Request, res: Response) {
+    res.json({
+      id: 1,
+      name: "Jorge",
+    });
+  }
+
+  static async delete(_req: Request, res: Response) {
+    res.json({
+      id: 1,
+      name: "Jorge",
+    });
   }
 }
 
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,8 +1,12 @@
-import { Router } from 'express';
-import UserController from '../controllers/user.controller';
+import { Router } from "express";
+import UserController from "../controllers/user.controller";
 
 const router = Router();
 
-router.get('/', UserController.getUsers);
+router.get("/", UserController.get);
+router.get("/:id", UserController.find);
+router.post("/", UserController.create);
+router.put("/:id", UserController.update);
+router.delete("/:id", UserController.delete);
 
 export default router;
